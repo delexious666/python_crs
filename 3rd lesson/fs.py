09d7f7e3b3bd8e5087f0176412957cbf822df71d
delexious666 <delexious666@gmail.com>
2023-02-13 04:00:42 +0600

    com2

---
+++ 3rd lesson/fs.py
@@ -0,0 +1,56 @@
+# a = 13
+#
+#if a == 13:
+#   print("yes")
+
+# a = int(input())
+# 
+# if a == 5:
+#     print("5")
+# elif a >= 10:
+#    print(">10")
+# elif a <= 3:
+#    print("<3")
+# else:
+#    print('wrong term')#
+
+# a = int(input())
+#
+# if a == 5 and a <=10:
+#    print("qte")
+# elif a == 10:
+#    print("10")
+# else:
+#    print("wrong term")#
+ 
+# a = int(input())
+# if a <= 13:
+#    print("nope")
+# else:
+#    if a >= 13:
+#        print("yes")
+#        b = int(input())
+#        if b == 10:
+#            print("done")
+#        else:
+#            print("go to the exit")#
+
+
+
+# Truth table(exemple) #
+
+# X      Y       NOT X      X OR Y      AND Y 
+
+# F      F         T          F           F #
+
+
+# a = int(input())
+# b = 666
+# c = 777
+
+# if b <= c and a <= c:
+#    a += 13
+#    if a == 26:
+#        print(c)
+#    else:
+#        print("nope") #
